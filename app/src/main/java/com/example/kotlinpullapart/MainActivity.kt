524518783165f8410edae3c122b0ea5c113fa9b4
Kevin Le <kevinle108@gmail.com>
2021-09-28 21:07:54 -0400

    working dependent spinners

---
+++ app/src/main/java/com/example/kotlinpullapart/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.kotlinpullapart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.kotlinpullapart.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val listParent = mutableListOf<String>(
+            "Animals",
+            "Birds",
+            "Flowers"
+        )
+        val arrayAdapterParent = ArrayAdapter(
+            this, R.layout.textview_blue, listParent
+        )
+        binding.spParent.adapter = arrayAdapterParent
+
+        val listChildAnimals = arrayListOf<String>(
+            "Tiger",
+            "Lion",
+            "Elephant",
+            "Monkey",
+            "Cow"
+        )
+        val listChildFlowers = arrayListOf<String>(
+            "Rose",
+            "Lotus"
+        )
+        val listChildBirds = arrayListOf<String>(
+            "Sparrow", "Eagle", "Peacock"
+        )
+
+        val childLists = listOf<List<String>>(listChildAnimals, listChildBirds, listChildFlowers)
+        val arrayAdapterChild = ArrayAdapter(this, R.layout.textview_red, listChildAnimals)
+        binding.spChild.adapter = arrayAdapterChild
+
+        binding.spParent.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext, listParent[position], Toast.LENGTH_LONG).show()
+
+                val adapterChild = ArrayAdapter(applicationContext, R.layout.textview_red, childLists[position])
+                binding.spChild.adapter = adapterChild
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+
+
+//        val spinnerYear = findViewById<Spinner>(R.id.spinnerYear)
+//        val spinnerMake = findViewById<Spinner>(R.id.spinnerMake)
+//        val spinnerModel = findViewById<Spinner>(R.id.spinnerModel)
+
+
+
+//        val spinnerCategories = listOf<Int>(
+//            R.array.years,
+//            R.array.makes,
+//            R.array.models
+//        )
+
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.years,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            spinnerYear.adapter = adapter
+//        }
+
+
+
+
+
+
+    }
+}
